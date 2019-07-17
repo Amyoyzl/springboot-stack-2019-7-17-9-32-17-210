e5888e7ae2ca5c443495e10405c9f9446e7a3c65
Amyoyzl <1421790940@qq.com>
2019-07-17 20:46:04 +0800

    Story1 AC2: get criminal by id

--- src/main/java/com/tw/apistackbase/controller/CriminalController.java
+++ src/main/java/com/tw/apistackbase/controller/CriminalController.java
@@ -3,10 +3,9 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.model.Criminal;
 import com.tw.apistackbase.repository.CriminalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/criminals")
@@ -19,6 +18,12 @@ public class CriminalController {
     public Criminal add(@RequestBody Criminal criminal){
         criminal.setTime(System.currentTimeMillis());
         return repository.save(criminal);
+    }
+
+    @GetMapping("/{id}")
+    public Criminal getById(@PathVariable long id) {
+        Optional<Criminal> optionalCriminal = repository.findById(id);
+        return optionalCriminal.isPresent() ? optionalCriminal.get() : null;
     }
 
 }
