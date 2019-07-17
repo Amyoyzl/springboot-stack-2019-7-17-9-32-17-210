44135bfd7d060032b718b01d48ff734294885b5d
Amyoyzl <1421790940@qq.com>
2019-07-17 21:05:39 +0800

    Story1 AC4: get criminals by name

--- src/main/java/com/tw/apistackbase/controller/CriminalController.java
+++ src/main/java/com/tw/apistackbase/controller/CriminalController.java
@@ -4,10 +4,12 @@ import com.tw.apistackbase.model.Criminal;
 import com.tw.apistackbase.repository.CriminalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/criminals")
@@ -31,6 +33,13 @@ public class CriminalController {
     @GetMapping
     public List<Criminal> getAll() {
         return repository.findAll(new Sort(Sort.Direction.DESC, "time"));
+    }
+
+    @GetMapping(params = "name")
+    public List<Criminal> getByName(@RequestParam String name) {
+        return repository.findAll().stream()
+                .filter(criminal -> criminal.getName().contains(name))
+                .collect(Collectors.toList());
     }
 
 }
