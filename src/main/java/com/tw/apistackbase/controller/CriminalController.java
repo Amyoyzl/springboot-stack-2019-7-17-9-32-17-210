1c150c9ac8456b3af106962f43f34039fd94e29f
Amyoyzl <1421790940@qq.com>
2019-07-17 20:53:01 +0800

    Story1 AC3: get criminals desc

--- src/main/java/com/tw/apistackbase/controller/CriminalController.java
+++ src/main/java/com/tw/apistackbase/controller/CriminalController.java
@@ -3,8 +3,10 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.model.Criminal;
 import com.tw.apistackbase.repository.CriminalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,6 +26,11 @@ public class CriminalController {
     public Criminal getById(@PathVariable long id) {
         Optional<Criminal> optionalCriminal = repository.findById(id);
         return optionalCriminal.isPresent() ? optionalCriminal.get() : null;
+    }
+
+    @GetMapping
+    public List<Criminal> getAll() {
+        return repository.findAll(new Sort(Sort.Direction.DESC, "time"));
     }
 
 }
